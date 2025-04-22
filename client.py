f207272df4271eb648d498343492bdf453796f41
Tobias-Henriksen <at101305@edu.aarhustech.dk>
2025-04-22 15:19:46 +0200

    Reworked the communication. Seperate server and client program. Missing connection currently.

--- client.py
+++ client.py
@@ -1,8 +1,62 @@
-import socket
+# Inspiration for first client draft. https://www.youtube.com/watch?v=_fx7FQ3SP0U&list=PLzMcBGfZo4-kR7Rh-7JCVDN8lm3Utumvq&index=1&ab_channel=TechWithTim
+import pygame
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 8080))
+width = 500
+height = 500
 
-print(client.recv(1024).decode('utf-8'))
-client.send('Welcome to Eksamensprojekt!'.encode('utf-8'))
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
 
+clientNumber = 0
+
+class Player():
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = (x, y, width, height)
+        self.vel = 3
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.x -= self.vel
+
+        if keys[pygame.K_RIGHT]:
+            self.x += self.vel
+
+        if keys[pygame.K_UP]:
+            self.y -= self.vel
+
+        if keys[pygame.K_DOWN]:
+            self.y += self.vel
+
+        self.rect = (self.x, self.y, self.width, self.height)
+
+def redrawWindow(win, player):
+    win.fill((255, 255, 255))
+    player.draw(win)
+    pygame.display.update()
+
+
+def main():
+    running = True
+    p = Player(3, 30, 20, 20, (0, 0, 255))
+    clock = pygame.time.Clock()
+
+    while running:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+        p.move()
+        redrawWindow(win, p)
+
+main()
--- server.py
+++ server.py
@@ -1,15 +1,45 @@
+# Inspiration for first server draft. https://www.youtube.com/watch?v=_whymdfq-R4&list=PLzMcBGfZo4-kR7Rh-7JCVDN8lm3Utumvq&index=3&ab_channel=TechWithTim
 import socket
+from _thread import *
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 8080))
+server = "" #indsæt IPv4 adresse
+port = 5555
 
-server.listen(1)
-u = 0
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Vælger at den skal forbinde med IPv4
+
+try:
+    s.bind( (server, port) )
+except socket.error as e:
+    print(e)
+
+s.listen(2) #Mængden af clients der kan connecte
+print("Waiting for a connection, server is ready...")
+
+
+def threaded_client(conn):
+
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received:", reply)
+                print("Sending:", reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+
 
 while True:
-    u = u + 1
-    client, addr = server.accept()
-    client.send('Welcome to Eksamensprojekt!'.encode('utf-8'))
-    print('Connected by', addr)
-    print(client.recv(1024).decode('utf-8'))
-    print(u)
+    conn, addr = s.accept() #Acceptere alle indgående connection anmodninger
+    print("Got connection from", addr) #Giver besked om at connection er modtaget og hvor den er fra.
+
+    start_new_thread(threaded_client, (conn,))
